5549b4e9182addf41b37061d5e177c658decf8cf
cerberus-zone <hadeshounds@cerberus.zone>
2022-02-27 02:15:14 -0600

    testing Makefile

--- app/genesis.go
+++ app/genesis.go
@@ -15,8 +15,6 @@ import (
 // object provided to it during init.
 type GenesisState map[string]json.RawMessage
 
-
-
 // NewDefaultGenesisState generates the default state for the application.
 func NewDefaultGenesisState(cdc codec.JSONCodec) GenesisState {
     return ModuleBasics.DefaultGenesis(cdc)
